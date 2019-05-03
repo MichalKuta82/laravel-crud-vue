2e14ffdd5babf7c4e18a22f93fef600ebe4572b2
Michal Kuta <michal@webmedia.ie>
2019-05-03 17:00:58 +0100

    Backend CRUD ready, npm installed

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Task;
+// use Symfony\Component\HttpFoundation\Response;
+
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tasks = Auth::user()->tasks;
+        // return response(['tasks' => $tasks->jsonSerialize()], Response::HTTP_OK);
+        return response()->json(['tasks' => $tasks], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'name' => 'required',
+            'body' => 'required',
+        ]);
+
+        $user = Auth::user();
+
+        $task = $user->tasks()->create([
+            'name' => $request->input('name'),
+            'body' => $request->input('body'),
+        ]);
+
+        return response()->json(['task' => $task, 'message' => 'Task has benn created'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $request->validate([
+            'name' => $request->input('name'),
+            'body' => $request->input('body'),
+        ]);
+
+        $task = $request->user()->tasks()->whereId($id)->update($request->all());
+
+        return response()->json(['task' => $task, 'message' => 'Task has been updated'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+        $task->delete();
+
+        return response()->json(['task' => $task, 'message' => 'Task has been deleted'], 200);
+    }
+}
