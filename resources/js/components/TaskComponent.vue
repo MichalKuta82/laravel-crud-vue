73b630c2fe72d69a43ced10ba51248ff19cc4bae
Michal Kuta <michal@webmedia.ie>
2019-05-13 14:53:39 +0100

    Loading feature and notfication added

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -2,7 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 offset-md-2">
+              <div v-if="!loading">
+                <img :src="image" class="rounded mx-auto d-block" alt="loader">
+              </div>
 
+            <div v-else>
                 <button class="btn btn-primary btn-block" @click="loadCreateModal">Add New Task</button>
                 <br>
                 <table class="table table-striped table-dark" v-if="tasks">
@@ -85,6 +89,7 @@
                   </div>
                 </div>
               </div>
+            </div>
 
             </div>    
         </div>
@@ -102,13 +107,17 @@
             tasks: [],
             uri:'http://laravel-crud-vue.local/tasks',
             errors: [],
-            new_update_task: []
+            new_update_task: [],
+            image: 'images/loader1.gif',
+            loading: false,
+            toastr: toastr.options = {"positionClass": "toast-top-full-width"}
           }
         },
         methods:{
           loadTasks(){
             axios.get(this.uri).then(response => {
-              this.tasks = response.data.tasks //assign response from the backend to an empty array tasks: []
+              this.tasks = response.data.tasks; //assign response from the backend to an empty array tasks: []
+              this.loading = true;
             });
           },
           loadCreateModal(){
@@ -124,6 +133,7 @@
               this.tasks.push(response.data.task);
               this.resetData();
               $("#create-modal").modal("hide");
+              toastr.success(response.data.message);
             }).catch(error => {
               this.errors = [];
               if (error.response.data.errors.name) {
@@ -138,8 +148,9 @@
             axios.patch(this.uri + "/" + this.new_update_task.id, {
               name: this.new_update_task.name,
               body: this.new_update_task.body
-            }).then(respose => {
+            }).then(response => {
               $("#update-modal").modal("hide");
+              toastr.success(response.data.message);
             }).catch(error => {
               if (error.response.data.errors.name) {
                 this.errors.push(error.response.data.errors.name[0]);
@@ -155,6 +166,7 @@
               axios.delete(this.uri + "/" + this.tasks[index].id)
                 .then(response => {
                   this.$delete(this.tasks, index); // Vue.delete(this.task, index)
+                  toastr.success(response.data.message);
                 }).catch(error => {
                   console.log("could not delete");
                 });
